8a111a105b0d7794b347c323895480e16bfb0fc2
Mikael de Jesus <mikaeljl824@gmail.com>
2024-09-19 20:39:22 -0300

    Adicionar arquivos de projeto.

---
+++ Leitura de senha/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Leitura_de_senha
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int senha = int.Parse(Console.ReadLine());
+
+            while (senha != 454545) {
+                Console.WriteLine("Senha invalida");
+                senha = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Acesso Permitido");
+
+        }
+    }
+}
